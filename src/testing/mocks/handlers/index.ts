404e654cba3bb47c6a21902482a4620fc9d06405
Luka Jaredic <luka.jaredic@effectiva.hr>
2026-05-17 23:29:05 +0200

    add mock backend

--- src/testing/mocks/handlers/index.ts
+++ src/testing/mocks/handlers/index.ts
@@ -5,9 +5,11 @@ import { env } from '@/config/env'
 import { networkDelay } from '../utils'
 
 import { authHandlers } from './auth'
+import { usersHandlers } from './users'
 
 export const handlers = [
   ...authHandlers,
+  ...usersHandlers,
   http.get(`${env.API_URL}/health`, async () => {
     await networkDelay()
     return HttpResponse.json({ ok: true })
