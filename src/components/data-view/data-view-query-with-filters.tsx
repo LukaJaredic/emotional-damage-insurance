c4f598cfcdc4ed18c7ebdb697a824903c4b6f9f0
Luka Jaredic <luka.jaredic@effectiva.hr>
2026-05-22 17:02:02 +0200

    hook DataView filters to searchparams

--- src/components/data-view/data-view-query-with-filters.tsx
+++ src/components/data-view/data-view-query-with-filters.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
 import type { DefaultValues, FieldValues } from 'react-hook-form'
+import { useSearchParams } from 'react-router-dom'
 
 import Filters, { type Filter } from '@/components/filters'
 import { cn } from '@/lib/utils'
@@ -29,6 +29,62 @@ function buildDefaultValues<T extends FieldValues>(
   ) as DefaultValues<T>
 }
 
+function readFilterValue<T extends FieldValues>(
+  searchParams: URLSearchParams,
+  filter: Filter<T>,
+) {
+  if (filter.type === 'select' && filter.isMultiple) {
+    return searchParams.getAll(filter.name)
+  }
+
+  return searchParams.get(filter.name) ?? ''
+}
+
+function buildFilterValues<T extends FieldValues>(
+  searchParams: URLSearchParams,
+  filters: Filter<T>[],
+): T {
+  return Object.assign(
+    buildDefaultValues(filters),
+    Object.fromEntries(
+      filters.map((filter) => [
+        filter.name,
+        readFilterValue(searchParams, filter),
+      ]),
+    ),
+  ) as T
+}
+
+function buildNextSearchParams<T extends FieldValues>(
+  currentSearchParams: URLSearchParams,
+  values: T,
+  filters: Filter<T>[],
+) {
+  const nextSearchParams = new URLSearchParams(currentSearchParams)
+
+  for (const filter of filters) {
+    nextSearchParams.delete(filter.name)
+
+    const value = values[filter.name]
+
+    if (Array.isArray(value)) {
+      for (const item of value as any[]) {
+        if (item) {
+          nextSearchParams.append(filter.name, item)
+        }
+      }
+
+      continue
+    }
+
+    if (typeof value === 'string' && value) {
+      nextSearchParams.set(filter.name, value)
+    }
+  }
+
+  return nextSearchParams
+}
+
 function DataViewQueryWithFilters<
   TItem extends Record<string, unknown>,
   TFilters extends FieldValues,
@@ -38,17 +94,23 @@ function DataViewQueryWithFilters<
   className,
   ...props
 }: DataViewQueryWithFiltersProps<TItem, TFilters>) {
-  const [filterValues, setFilterValues] = useState<TFilters>(
-    () => buildDefaultValues(filters) as TFilters,
-  )
+  const [searchParams, setSearchParams] = useSearchParams()
+  const filterValues = buildFilterValues(searchParams, filters)
+
   const query = useQuery(filterValues)
+
+  function handleFilterChange(values: TFilters) {
+    setSearchParams(buildNextSearchParams(searchParams, values, filters), {
+      replace: true,
+    })
+  }
 
   return (
     <div className={cn(className, 'flex min-h-0 flex-1 flex-col gap-4')}>
       <Filters
         filters={filters}
         defaultValues={filterValues as DefaultValues<TFilters>}
-        onChange={setFilterValues}
+        onChange={handleFilterChange}
       />
 
       <div className="min-h-0 flex-1">
