4459641e5ca4a3dcd4ffed98c07062a1f2b7f2ad
Luka Jaredic <luka.jaredic@effectiva.hr>
2026-05-28 13:49:33 +0200

    add delete user functionality
    add confirmdialog component

--- src/features/users/api/delete-user.ts
+++ src/features/users/api/delete-user.ts
@@ -1,6 +1,8 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'sonner'
 
-import { queryKeys } from '@/config'
+import { paths, queryKeys } from '@/config'
 import { api } from '@/lib'
 
 import type { DeleteUserAction } from '../types/user-api.types'
@@ -11,11 +13,14 @@ export async function deleteUser({ userId }: DeleteUserAction) {
 
 export function useDeleteUser() {
   const queryClient = useQueryClient()
+  const navigate = useNavigate()
 
   return useMutation({
     mutationFn: deleteUser,
     onSuccess: () => {
       void queryClient.invalidateQueries({ queryKey: queryKeys.users.all() })
+      navigate(paths.users.getHref())
+      toast.success('User deleted successfully')
     },
   })
 }
--- src/features/users/components/detail/user-detail.tsx
+++ src/features/users/components/detail/user-detail.tsx
@@ -1,10 +1,11 @@
-import { PencilIcon } from '@phosphor-icons/react'
+import { PencilIcon, TrashIcon } from '@phosphor-icons/react'
 
 import { PageLayout } from '@/components/layout'
-import { QueryError, QueryLoading } from '@/components/ui'
+import { ConfirmDialog, QueryError, QueryLoading } from '@/components/ui'
 import Button from '@/components/ui/shadcn/button'
 import { useUserDetail } from '@/features/users/api/get-user'
 import type { UserRole } from '@/types'
+import { useDeleteUser } from '@features/users/api/delete-user'
 import { roleLabels } from '@features/users/utils/user-labels'
 
 import UserFormDialog from '../form/user-form-dialog'
@@ -22,6 +23,7 @@ function formatRoles(roles: UserRole[]) {
 
 function UserDetail({ userId }: UserDetailProps) {
   const query = useUserDetail({ userId })
+  const deleteMutation = useDeleteUser()
 
   if (query.isPending) {
     return <QueryLoading label="Loading user details..." />
@@ -43,11 +45,33 @@ function UserDetail({ userId }: UserDetailProps) {
       heading={`${user.firstName} ${user.lastName}`}
       description={formatRoles(user.roles)}
       actions={() => (
-        <UserFormDialog user={user}>
-          <Button>
-            <PencilIcon /> Edit this user
-          </Button>
-        </UserFormDialog>
+        <>
+          <UserFormDialog user={user}>
+            <Button>
+              <PencilIcon /> Edit this user
+            </Button>
+          </UserFormDialog>
+          <ConfirmDialog
+            title={
+              <>
+                Delete{' '}
+                <strong className="bg-primary px-1">
+                  {user.firstName} {user.lastName}
+                </strong>
+                ?
+              </>
+            }
+            description="Are you sure you want to delete this user? This action cannot be undone."
+            confirmLabel="Delete"
+            variant="destructive"
+            onConfirm={async () => deleteMutation.mutateAsync({ userId })}
+          >
+            <Button variant="destructive" disabled={!user}>
+              <TrashIcon />
+              Delete this user
+            </Button>
+          </ConfirmDialog>
+        </>
       )}
     >
       <div className="flex min-h-0 w-full flex-col gap-6 *:last:flex-1 xl:flex-row xl:items-start xl:*:first:basis-125">
