b4f9926a2b9007b395cecf806cc64a648e222e73
Luka Jaredic <luka.jaredic@effectiva.hr>
2026-06-23 14:22:48 +0200

    fix app error boundary

--- src/components/errors/error-boundary.tsx
+++ src/components/errors/error-boundary.tsx
@@ -1,6 +1,7 @@
 import {
   getErrorMessage,
   ErrorBoundary as ReactErrorBoundary,
+  type ErrorBoundaryPropsWithRender,
 } from 'react-error-boundary'
 
 import Error, { type ErrorProps } from './error'
@@ -9,27 +10,32 @@ type ErrorBoundaryProps = {
   children: React.ReactNode
   variant?: ErrorProps['variant']
   actions?: ErrorProps['actions']
+  fallBackRender?: ErrorBoundaryPropsWithRender['fallbackRender']
 }
 
 function ErrorBoundary({
   children,
   variant = 'container',
   actions = 'back-home-retry',
+  fallBackRender,
 }: ErrorBoundaryProps) {
   return (
     <ReactErrorBoundary
-      fallbackRender={(error) => (
-        <Error
-          status="ERROR"
-          title="Something went wrong"
-          description={
-            getErrorMessage(error.error) || 'An unexpected error occurred.'
-          }
-          variant={variant}
-          actions={actions}
-          onActionClick={error.resetErrorBoundary}
-        />
-      )}
+      fallbackRender={
+        fallBackRender ||
+        ((error) => (
+          <Error
+            status="ERROR"
+            title="Something went wrong"
+            description={
+              getErrorMessage(error.error) || 'An unexpected error occurred.'
+            }
+            variant={variant}
+            actions={actions}
+            onActionClick={error.resetErrorBoundary}
+          />
+        ))
+      }
       onError={(error, info) => {
         // TODO: Sentry logging
         console.error('ErrorBoundary caught an error:', error, info)
